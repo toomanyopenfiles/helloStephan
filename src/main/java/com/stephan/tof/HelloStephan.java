cefc910256adb0651d7f11fcb6cd8b1ca6709eb0
gaosong <gaosong@youku.com>
2016-02-01 01:49:30 +0800

    提交项目框架

---
+++ src/main/java/com/stephan/tof/HelloStephan.java
@@ -0,0 +1,10 @@
+package com.stephan.tof;
+
+public class HelloStephan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello stephan~");
+	}
+
+}
